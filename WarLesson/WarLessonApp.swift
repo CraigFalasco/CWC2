ebd0e6a8c57e39eab76b574708d6455490920f24
CraigFalasco <craigfalasco>
2023-01-15 08:52:17 -0700

    Initial Commit

---
+++ WarLesson/WarLessonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarLessonApp.swift
+//  WarLesson
+//
+//  Created by Craig on 12/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WarLessonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
