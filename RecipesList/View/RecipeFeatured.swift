34ef93f7b562b9251e30d4d872ffce41f4c740b8
CraigFalasco <craigfalasco>
2023-01-21 09:17:47 -0700

    add featured recipe cards

--- RecipesList/View/RecipeFeatured.swift
+++ RecipesList/View/RecipeFeatured.swift
@@ -12,12 +12,66 @@ struct RecipeFeatured: View {
     @EnvironmentObject var model:RecipeVuModel
     
     var body: some View {
-        Text("Featured View")
+        
+        VStack {
+            
+            Text("Featured Recipes")
+                .font(.largeTitle)
+                .padding(10)
+                .padding(.top, 40)
+                
+            
+            GeometryReader { geo in
+                
+                TabView {
+                    
+                    ForEach (0..<model.recipes.count, id: \.self) { index in
+                        if model.recipes[index].featured == true {
+                            
+                            ZStack {
+                                
+                                Rectangle()
+                                    .foregroundColor(.white)
+                                    
+                                VStack(spacing: 0) {
+                                    Image(model.recipes[index].image)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .clipped()
+
+                                    Text(model.recipes[index].name)
+                                        .font(.title3)
+                                        .padding(10)
+ 
+                                }
+                            }
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                            .cornerRadius(10)
+                            .shadow(color: .black, radius: 10, x: -5, y: 5)
+                        }
+                    }
+                }
+                // this makes the tab view swipe-able
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                // this adds a background to the dots so they are visible
+                .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            }
+            
+            VStack(alignment: .leading ,spacing: 10) {
+                Text("Prep Time: ")
+                Text("Cook Time: ")
+                Text("Total Time: ")
+                Text("Servings: ")
+            }
+            .padding(.bottom, 20.0)
+        }
+        
     }
 }
 
 struct RecipeFeatured_Previews: PreviewProvider {
     static var previews: some View {
         RecipeFeatured()
+            .environmentObject(RecipeVuModel())
     }
 }
