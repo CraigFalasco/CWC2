9a80e85a00bb9210efa2567d049fc2e325cc0d61
CraigFalasco <craigfalasco>
2023-01-24 21:41:29 -0700

    initial commit

--- M4Ch7Cards/ContentView.swift
+++ M4Ch7Cards/ContentView.swift
@@ -8,15 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    
+    var color : Color {
+        return Color (
+            .sRGB,
+            red: Double.random(in: 0..<1),
+            green: Double.random(in: 0..<1),
+            blue: Double.random(in: 0..<1),
+            opacity: 0.75)
     }
+            
+        
+        var body: some View {
+            
+            TabView {
+
+                ForEach (1..<51) { x in
+                    GeometryReader { geometry in
+                        
+                        ZStack {
+                            Rectangle()
+                                .foregroundColor(color)
+                                .cornerRadius(10)
+                                .shadow(radius: 10)
+                            
+                            Image(systemName: "\(x).circle")
+                                .resizable()
+                                .scaledToFit()
+                                .padding()
+                        }
+                        .frame(height: max(geometry.size.height - 50, 100))
+
+                    }
+                }
+                .padding()
+
+            }
+            .tabViewStyle(PageTabViewStyle())
+            .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+            
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
