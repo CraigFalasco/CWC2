5440775f0aa639378e6fef97291961978478a2ab
CraigFalasco <craigfalasco>
2023-01-26 07:47:51 -0700

    Create cards getting gradually taller

--- M4Ch8Cards/ContentView.swift
+++ M4Ch8Cards/ContentView.swift
@@ -9,13 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        ScrollView {
+            LazyVStack(alignment: .leading) {
+                ForEach (1..<51) { x in
+                    Rectangle()
+                        .fill(RadialGradient(
+                            colors: ([.teal, .blue]),
+                            center: UnitPoint(x: 0.5, y: 0.5),
+                            startRadius: 0,
+                            endRadius: 200))
+                        .frame(height: pow(1.1, CGFloat(x)))
+                        .cornerRadius(10)
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
