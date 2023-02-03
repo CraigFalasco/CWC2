0cfd5173cf21cc828b37f85af930064580f86f08
CraigFalasco <craigfalasco>
2023-02-03 13:10:57 -0700

    Initial Commit

---
+++ M4FinalChallenge/M4FinalChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4FinalChallengeApp.swift
+//  M4FinalChallenge
+//
+//  Created by Craig on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct M4FinalChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
